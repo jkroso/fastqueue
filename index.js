2a4fae6d36bfefb0672e96a239cdb9b46ae539d7
jkroso <jkroso@gmail.com>
2013-06-24 12:38:52 +1200

    add remove() and toJSON()

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = Queue
 
-function Queue() {
+function Queue(){
   this.head = []
   this.tail = []
   this.index = 0
@@ -14,7 +14,7 @@ function Queue() {
  * @return {x}
  */
 
-Queue.prototype.shift = function () {
+Queue.prototype.shift = function(){
 	// swap head for tail
   if (this.index >= this.headLength) {
     var t = this.head
@@ -40,9 +40,11 @@ Queue.prototype.shift = function () {
  * @return {this}
  */
 
-Queue.prototype.unshift = function (item) {
-	if (this.index === 0) this.head.unshift(item)
-	else this.head[--this.index] = item
+Queue.prototype.unshift = function(item){
+	if (this.index === 0) {
+		this.head.unshift(item)
+		this.headLength++
+	} else this.head[--this.index] = item
   this.length++
   return this
 }
@@ -54,8 +56,48 @@ Queue.prototype.unshift = function (item) {
  * @return {this}
  */
 
-Queue.prototype.push = function (item) {
+Queue.prototype.push = function(item){
   this.length++
   this.tail.push(item)
   return this
+}
+
+/**
+ * remove all instances of `value` from the queue
+ * 
+ * @param {x} value
+ * @return {Boolean}
+ */
+
+Queue.prototype.remove = function(value){
+	var a = remove(this.head, value, this.index)
+	var b = remove(this.tail, value, 0)
+	if (a || b) {
+		this.length -= a + b 
+		this.headLength = this.head.length
+		return true
+	}
+	return false
+}
+
+function remove(arr, value, i){
+	var removed = 0
+	while (i < arr.length) {
+		if (arr[i] === value) {
+			arr.splice(i, 1)
+			removed++
+		} else i++
+	}
+	return removed
+}
+
+/**
+ * for JSON.stringify
+ * 
+ * @return {Array}
+ * @api private
+ */
+
+Queue.prototype.toJSON = function(){
+	return this.head.slice(this.index).concat(this.tail)
 }
